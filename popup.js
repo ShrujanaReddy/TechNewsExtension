fff1a5ecb663c0431936d274690f5491329ff901
ShrujanaReddy <130744023+ShrujanaReddy@users.noreply.github.com>
2023-08-19 19:21:15 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const fetchButton = document.getElementById('fetchNews');
+  const topicsSelect = document.getElementById('topics');
+  const newsContainer = document.getElementById('newsContainer');
+
+  fetchButton.addEventListener('click', fetchNews);
+
+  async function fetchNews() {
+    const selectedTopics = Array.from(topicsSelect.options)
+      .filter(option => option.selected)
+      .map(option => option.value);
+
+    const apiKey = '9050c29cf007dbedc79f54ac5d794dc2'; // Replace with your GNews API key
+    const apiUrl = `https://gnews.io/api/v4/search?lang=en&q=${selectedTopics.join('+')}&token=${apiKey}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      newsContainer.innerHTML = '';
+
+      data.articles.forEach(article => {
+        const articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
+
+        const articleLink = document.createElement('a');
+        articleLink.href = article.url;
+        articleLink.target = '_blank';
+        articleLink.textContent = article.title;
+
+        const source = document.createElement('p');
+        source.textContent = `Source: ${article.source.name}`;
+
+        articleDiv.appendChild(articleLink);
+        articleDiv.appendChild(source);
+        newsContainer.appendChild(articleDiv);
+      });
+    } catch (error) {
+      console.error('Error fetching news:', error);
+    }
+  }
+});
